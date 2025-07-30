b2b2b923c55076c1f176aa10e20c478dbd59772b
Raymart Magallanes <magallanesraymart@gmail.com>
2025-07-30 21:22:23 +0800

    added welcome dashboard

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.guest')
+
+@section('content')
+    <div class="min-h-screen flex flex-col justify-between items-center px-4 py-10">
+        <!-- Centered text -->
+        <div class="text-center mt-20">
+            <h3 class="text-3xl font-semibold">Welcome to</h3>
+            <h1 class="text-5xl font-bold mt-2">{{ config('app.name') }}</h1>
+        </div>
+
+        <!-- Button at the bottom -->
+        <a href="{{ route('ar.view') }}"
+            class="bg-primary hover:bg-white hover:text-primary text-white py-2 px-6 rounded-4xl shadow-lg border border-primary transition-all duration-300">
+            Start Campus Tour
+        </a>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,15 @@
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\LogInController;
 use App\Http\Controllers\Admin\ProfileController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TourController;
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/', [HomeController::class, 'index'])->name('index');
+
+
+Route::get('/scan-marker', [TourController::class, 'index'])->name('ar.view');
 
 // Admin login (GET and POST)
 Route::get('/admin', [LogInController::class, 'showLoginForm'])->name('login');
