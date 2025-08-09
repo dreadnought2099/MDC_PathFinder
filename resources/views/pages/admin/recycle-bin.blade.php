245555f2ce010bbf7c6fc648112ac65b74b7abd8
Raymart Magallanes <magallanesraymart@gmail.com>
2025-08-09 16:03:35 +0800

    added collapsible sections for recycle bin

--- resources/views/pages/admin/recycle-bin.blade.php
+++ resources/views/pages/admin/recycle-bin.blade.php
@@ -1,37 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mx-auto p-4">
+<div x-data="{ tab: 'rooms' }" class="container mx-auto p-4">
+  <nav class="mb-4 flex space-x-4 border-b">
+    <button @click="tab = 'rooms'" :class="{ 'border-primary border-b-2': tab === 'rooms' }" class="pb-2">
+      Trashed Rooms
+    </button>
+    <button @click="tab = 'staff'" :class="{ 'border-primary border-b-2': tab === 'staff' }" class="pb-2">
+      Trashed Staff
+    </button>
+  </nav>
 
+  <div x-show="tab === 'rooms'">
     <x-recycle-bin-table
         :items="$rooms"
         route-prefix="room"
         title="Trashed Rooms"
         empty-message="No trashed rooms found."
     />
+  </div>
 
+  <div x-show="tab === 'staff'">
     <x-recycle-bin-table
         :items="$staffs"
         route-prefix="staff"
         title="Trashed Staff Members"
         empty-message="No trashed staff members found."
     />
-
+  </div>
 </div>
-
-<script>
-    function showModal(id) {
-        document.getElementById(id).classList.remove('hidden');
-    }
-
-    function hideModal(id) {
-        document.getElementById(id).classList.add('hidden');
-    }
-
-    function closeModal(event, modal) {
-        if (event.target === modal) {
-            modal.classList.add('hidden');
-        }
-    }
-</script>
 @endsection
