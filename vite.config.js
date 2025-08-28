70c0f867fab4a9b87990c493edde4b447a69e980
Raymart Magallanes <magallanesraymart@gmail.com>
2025-08-28 16:30:30 +0800

    fixed the error where font Anton is not available in some browsers like Brave

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
     // server: {
-    //     host: "192.168.1.28", 
-    //     port: 5173, 
+    //     host: "192.168.1.28",
+    //     port: 5173,
     //     hmr: {
-    //         host: "192.168.1.28", 
+    //         host: "192.168.1.28",
     //     },
     // },
     plugins: [
@@ -16,5 +17,13 @@ export default defineConfig({
             refresh: true,
         }),
         tailwindcss(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "resources/fonts/*",
+                    dest: "fonts", // copied to public/build/fonts/
+                },
+            ],
+        }),
     ],
 });
