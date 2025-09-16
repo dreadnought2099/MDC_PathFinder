12b14bc67d6cdfe080fb587f0039755a90d5696f
Raymart Magallanes <magallanesraymart@gmail.com>
2025-09-16 09:44:17 +0800

    removed italic for paragraph

--- resources/views/pages/client/about/index.blade.php
+++ resources/views/pages/client/about/index.blade.php
@@ -47,7 +47,7 @@
             title="Scan office to know more" />
 
         <div class="min-h-[70vh] flex flex-col items-center justify-center space-y-12 px-6 md:px-16 py-12">
-            @foreach (['about' => 'What is <span class="text-primary">' . config('app.name') . '</span>?', 'overview' => '<span class="text-primary">Overview</span> Content'] as $key => $title)
+            @foreach (['about' => 'What is <span class="text-primary">' . config('app.name') . '</span>?', 'overview' => '<span class="text-primary">Overview</span> of Content'] as $key => $title)
                 <div class="w-full max-w-3xl mx-auto md:px-8">
                     <h1
                         class="text-3xl md:text-4xl font-bold text-dark leading-snug dark:text-gray-300 text-center md:text-left">
@@ -55,7 +55,7 @@
                     </h1>
                     <div class="mt-6 md:mt-8 space-y-6 text-lg text-gray-700 md:text-left text-center">
                         @foreach ($aboutData[$key]['content'] as $content)
-                            <p class="italic font-sofia leading-relaxed md:leading-loose tracking-wide dark:text-gray-300">
+                            <p class="font-sofia leading-relaxed md:leading-loose tracking-wide dark:text-gray-300">
                                 {{ $content }}
                             </p>
                         @endforeach
