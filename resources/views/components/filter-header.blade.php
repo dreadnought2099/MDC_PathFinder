ea6f208b867ac2df500640e4a0079b2f3729d673
Raymart Magallanes <magallanesraymart@gmail.com>
2025-10-10 00:01:01 +0800

    input type design changes

--- resources/views/components/filter-header.blade.php
+++ resources/views/components/filter-header.blade.php
@@ -24,7 +24,7 @@
     <form @submit.prevent="submitSearch" class="flex items-center gap-2 w-full sm:w-auto">
         <input type="text" x-model="search" value="{{ request('search', '') }}"
             placeholder="Search {{ $placeholder }}..." @input.debounce.300ms="submitSearch()"
-            class="font-sofia border border-gray-300 rounded-lg px-3 py-2 w-full sm:w-64 focus:outline-none focus:ring focus:ring-primary/40 dark:bg-gray-800 dark:text-white">
+            class="font-sofia border border-primary rounded-md px-3 py-2 w-full sm:w-64 outline-none focus:ring focus:ring-primary focus:border-primary dark:bg-gray-800 dark:text-white">
 
         <template x-if="search">
             <button type="button" @click="clearSearch"
