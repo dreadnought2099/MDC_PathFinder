7eb3a7037c43d52aaa81b6c166fa937bcb83927a
Raymart Magallanes <magallanesraymart@gmail.com>
2025-11-25 20:16:53 +0800

    refactor: changed from url to laravel route name

--- resources/views/errors/429.blade.php
+++ resources/views/errors/429.blade.php
@@ -6,8 +6,8 @@
 
 @section('content')
     <div class="mt-8">
-        <a href="{{ url('/') }}"
-            onclick="event.preventDefault(); history.length > 1 ? history.back() : window.location='{{ url('/') }}';"
+        <a href="{{ route('index') }}"
+            onclick="event.preventDefault(); history.length > 1 ? history.back() : window.location='{{ route('index') }}';"
             class="w-full bg-primary text-white px-4 py-2 rounded-full border-2 border-primary duration-300 transition-all ease-in-out mt-4 disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer hover:bg-white hover:text-primary dark:hover:bg-gray-800 shadow-primary-hover">
             Go Back
         </a>
